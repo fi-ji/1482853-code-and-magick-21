202647effb3c31920c822e34648a35fcdc83cdf0
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-09-19 22:49:28 +0200

    удаляет лишние двоеточия

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,21 @@
+'use strict';
+
 const fireballSize = 22;
 const wizardSpeed = 3;
 const wizardWidth = 70;
 
 function getFireballSpeed(isMovingLeft) {
   return isMovingLeft ? 2 : 5;
-};
+}
 
 function getWizardHeight() {
   return 1.337 * wizardWidth;
-};
+}
 
 function getWizardX(fieldWidth) {
   return (fieldWidth - wizardWidth) / 2;
-};
+}
 
 function getWizardY(fieldHeight) {
   return fieldHeight / 3;
-};
+}
