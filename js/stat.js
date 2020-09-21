f129fc8bcd473aae068efa08485ef35e7e383bf5
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-09-21 13:21:08 +0200

    рефакторит код

--- js/stat.js
+++ js/stat.js
@@ -17,30 +17,29 @@ const BAR_HEIGHT = CLOUD_HEIGHT - PADDING * 2 - TEXT_HEIGHT * 3;
 
 // Настройка параметров
 
-function getFireballSpeed(isMovingLeft) {
-  return isMovingLeft ? 2 : 5;
-}
+const getFireballSpeed = (isMovingLeft) => isMovingLeft ? 2 : 5;
 
-function getWizardHeight() {
-  return 1.337 * wizardWidth;
-}
+const getWizardHeight = () => 1.337 * wizardWidth;
 
-function getWizardX(fieldWidth) {
-  return (fieldWidth - wizardWidth) / 2;
-}
+const getWizardX = (fieldWidth) => (fieldWidth - wizardWidth) / 2;
 
-function getWizardY(fieldHeight) {
-  return fieldHeight / 3;
-}
+const getWizardY = (fieldHeight) => fieldHeight / 3;
 
 // Статистика
 
-function renderCloud(ctx, x, y, color) {
+const renderCloud = (ctx, x, y, color) => {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-function getMaxElement(arr) {
+const renderHoorayText = (ctx) => {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', CLOUD_X + PADDING, CLOUD_Y + PADDING);
+  ctx.fillText('Список результатов:', CLOUD_X + PADDING, CLOUD_Y + PADDING + TEXT_HEIGHT);
+};
+
+const getMaxElement = (arr) => {
   let maxElement = arr[0];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -48,25 +47,25 @@ function getMaxElement(arr) {
     }
   }
   return maxElement;
-}
+};
 
-window.renderStatistics = function (ctx, names, times) {
+const renderBars = (ctx, players, scores, maxScore) => {
+  for (let i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT);
+    ctx.fillText(Math.round(scores[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT - (BAR_HEIGHT * scores[i]) / maxScore - TEXT_HEIGHT / 2);
+
+    ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : `hsl(235deg, ${Math.random() * 100}%, 27%)`;
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT, BAR_WIDTH, (-BAR_HEIGHT * scores[i]) / maxScore);
+  }
+};
+
+window.renderStatistics = (ctx, names, times) => {
   renderCloud(ctx, CLOUD_X + SHADE_OFFSET, CLOUD_Y + SHADE_OFFSET, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + PADDING, CLOUD_Y + PADDING);
-  ctx.fillText('Список результатов:', CLOUD_X + PADDING, CLOUD_Y + PADDING + TEXT_HEIGHT);
+  renderHoorayText(ctx);
 
   let maxTime = getMaxElement(times);
 
-  for (let i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - TEXT_HEIGHT / 2);
-
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : `hsl(235deg, ${Math.random() * 100}%, 27%)`;
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT, BAR_WIDTH, (-BAR_HEIGHT * times[i]) / maxTime);
-  }
+  renderBars(ctx, names, times, maxTime);
 };
